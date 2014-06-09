924e64ef1a182108c7103744f010c62de01ce5bb
davidcarrera <davidcarrera@users.noreply.github.com>
2014-06-09 14:08:29 +0200

    Create mqtt-and-stomp-bridge--per-stream.js
    Added overloading of topics for the responses ("/topic/API_TOKEN.to", "/topic/API_TOKEN.to.SOID" and "/topic/API_TOKEN.to.SOID.stream")

---
+++ mqtt-and-stomp-bridge--per-stream.js
@@ -0,0 +1,29 @@
+var Stomp = require('stompjs');
+var api = require('restler');
+ 
+// Use raw TCP sockets
+var client = Stomp.overTCP('api.servioticy.com', 1883);
+// uncomment to print out the STOMP frames
+//client.debug = console.log;
+ 
+client.connect('compose', 'shines', function(frame) {
+    client.subscribe('/topic/*.from', function(message) {
+ 
+        var request = JSON.parse(message.body);
+        console.log("Going for a " + request.meta.method);
+        console.log("Posted data " + JSON.stringify(request.body));
+        api.json("http://api.servioticy.com" + request.meta.url,
+                request.body,
+                {headers: {'Content-Type': 'application/json', 'Authorization': request.meta.authorization}},
+                request.meta.method
+        ).on('complete', function(data, response) {
+             // send back the identifier if any, allowing the requesting client to match the response        
+            data.messageId = request.meta.messageId || null;
+            var urlparts = request.meta.url.split("/");
+            client.send('/topic/' + request.meta.authorization + ".to", {}, JSON.stringify(data));
+            client.send('/topic/' + request.meta.authorization + ".to." + urlparts[urlparts.length-3], {}, JSON.stringify(data));
+            client.send('/topic/' + request.meta.authorization + ".to." + urlparts[urlparts.length-3] + "." + urlparts[urlparts.length-1], {}, JSON.stringify(data));
+            console.log("result: " + JSON.stringify(data));
+        });
+    });
+});
